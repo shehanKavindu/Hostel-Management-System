307e3b8ac88e5d0559fcc289e297ff09c0520c89
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 00:10:17 +0530

    RegisterForm.fxml

---
+++ src/lk/HostelManagementSystem/Controller/RegisterFormController.java
@@ -0,0 +1,20 @@
+package lk.HostelManagementSystem.Controller;
+
+import javafx.event.ActionEvent;
+
+public class RegisterFormController {
+    public void btnAdd(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdate(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete(ActionEvent actionEvent) {
+    }
+
+    public void btnNewStudentID(ActionEvent actionEvent) {
+    }
+
+    public void cmbRoomTypeIDOnAction(ActionEvent actionEvent) {
+    }
+}
