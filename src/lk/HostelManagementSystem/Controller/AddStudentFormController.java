d31bc3831d5a40126c954419375349ec4ddd23b8
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-20 22:16:38 +0530

    AddStudentForm.fxml

--- src/lk/HostelManagementSystem/Controller/AddStudentFormController.java
+++ src/lk/HostelManagementSystem/Controller/AddStudentFormController.java
@@ -1,4 +1,8 @@
 package lk.HostelManagementSystem.Controller;
 
+import javafx.event.ActionEvent;
+
 public class AddStudentFormController {
+    public void btnADD(ActionEvent actionEvent) {
+    }
 }
