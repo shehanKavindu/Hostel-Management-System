3b2ce8da00d96d469b7f69dfc87f79aea11fc897
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 12:49:51 +0530

    SettingForm.fxml

--- src/lk/HostelManagementSystem/Controller/SettingFormController.java
+++ src/lk/HostelManagementSystem/Controller/SettingFormController.java
@@ -1,4 +1,8 @@
 package lk.HostelManagementSystem.Controller;
 
+import javafx.event.ActionEvent;
+
 public class SettingFormController {
+    public void btnChange(ActionEvent actionEvent) {
+    }
 }
