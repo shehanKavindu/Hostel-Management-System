7dc689cf08ca33ed3c4a21b7b812aae4f4024e86
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 13:11:02 +0530

    StudentManagementForm.fxml

--- src/lk/HostelManagementSystem/Controller/StudentManagementFormController.java
+++ src/lk/HostelManagementSystem/Controller/StudentManagementFormController.java
@@ -3,15 +3,13 @@ package lk.HostelManagementSystem.Controller;
 import javafx.event.ActionEvent;
 
 public class StudentManagementFormController {
-    public void btnSearchOnAction(ActionEvent actionEvent) {
+
+    public void btnAdd(ActionEvent actionEvent) {
     }
 
-    public void btnAddOnAction(ActionEvent actionEvent) {
+    public void btnUpdate(ActionEvent actionEvent) {
     }
 
-    public void btnUpdateOnAction(ActionEvent actionEvent) {
-    }
-
-    public void bntDeleteOnAction(ActionEvent actionEvent) {
+    public void btnDelete(ActionEvent actionEvent) {
     }
 }
