2158133f441c9bdd58b805a55a3a100ff8c1951c
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 19:53:51 +0530

    PayementBO

--- src/lk/HostelManagementSystem/bo/custom/PayementBO.java
+++ src/lk/HostelManagementSystem/bo/custom/PayementBO.java
@@ -1,4 +1,10 @@
 package lk.HostelManagementSystem.bo.custom;
 
-public interface PayementBO {
+import lk.HostelManagementSystem.bo.SuperBO;
+import lk.HostelManagementSystem.dto.CustomDTO;
+
+import java.util.ArrayList;
+
+public interface PayementBO extends SuperBO{
+    ArrayList<CustomDTO> getAllPendingPayementStudent();
 }
