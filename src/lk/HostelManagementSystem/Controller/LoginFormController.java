9bcb66609446ee7fc5727e2327ff2f5857aa938b
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 14:25:08 +0530

    LoginForm

--- src/lk/HostelManagementSystem/Controller/LoginFormController.java
+++
@@ -1,8 +0,0 @@
-package lk.HostelManagementSystem.Controller;
-
-import javafx.event.ActionEvent;
-
-public class LoginFormController {
-    public void btnLoginOnAction(ActionEvent actionEvent) {
-    }
-}
