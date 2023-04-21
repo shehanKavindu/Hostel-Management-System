bb94e092e62cd41a3ff424de124ee03dc0776796
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 18:40:52 +0530

    UserBOImpl

--- src/lk/HostelManagementSystem/bo/custom/impl/UserBOImpl.java
+++ src/lk/HostelManagementSystem/bo/custom/impl/UserBOImpl.java
@@ -1,4 +1,14 @@
 package lk.HostelManagementSystem.bo.custom.impl;
 
-public class UserBOImpl {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+import lk.HostelManagementSystem.bo.custom.UserBO;
+
+public class UserBOImpl implements UserBO {
+    public UserBOImpl() {
+    }
 }
