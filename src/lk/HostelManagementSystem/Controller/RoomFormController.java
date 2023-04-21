dfa762ff6a092308d1876025799263585890c8b3
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 16:34:44 +0530

    RoomForm.fxml

--- src/lk/HostelManagementSystem/Controller/RoomFormController.java
+++ src/lk/HostelManagementSystem/Controller/RoomFormController.java
@@ -1,4 +1,17 @@
 package lk.HostelManagementSystem.Controller;
 
+import javafx.event.ActionEvent;
+
 public class RoomFormController {
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
 }
