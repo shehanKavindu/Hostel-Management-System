f723476ccca8ba1fa19965ecf30b8ddab23fd884
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 19:59:46 +0530

    RegistrationForm.fxml

--- src/lk/HostelManagementSystem/bo/custom/ReservationrBO.java
+++ src/lk/HostelManagementSystem/bo/custom/ReservationrBO.java
@@ -1,4 +1,24 @@
 package lk.HostelManagementSystem.bo.custom;
 
-public interface ReservationrBO {
+import lk.HostelManagementSystem.bo.SuperBO;
+import lk.HostelManagementSystem.dto.ReservationDTO;
+import lk.HostelManagementSystem.dto.RoomDTO;
+
+import java.util.ArrayList;
+
+public interface ReservationrBO extends SuperBO {
+    RoomDTO getRoom(String var1);
+
+    int getNotAvailableRoomCount(String var1);
+
+    boolean saveRegistration(ReservationDTO var1);
+
+    boolean deleteRegistration(ReservationDTO var1);
+
+    boolean updateRegistration(ReservationDTO var1);
+
+    ArrayList<ReservationDTO> getAll();
+
+    String getLastId();
+}
 }
