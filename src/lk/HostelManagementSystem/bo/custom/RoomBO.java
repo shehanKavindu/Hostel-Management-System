1e0b4ee7174a32d768c8bb749fc490701500560e
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-21 20:04:52 +0530

    RoomBO

--- src/lk/HostelManagementSystem/bo/custom/RoomBO.java
+++ src/lk/HostelManagementSystem/bo/custom/RoomBO.java
@@ -1,4 +1,19 @@
 package lk.HostelManagementSystem.bo.custom;
 
-public interface RoomBO {
-}
+import lk.HostelManagementSystem.bo.SuperBO;
+import lk.HostelManagementSystem.dto.RoomDTO;
+
+import java.util.ArrayList;
+
+public interface RoomBO extends SuperBO {
+
+    public interface RoomBO extends SuperBO {
+        boolean saveRoom(RoomDTO var1);
+
+        boolean deleteRoom(RoomDTO var1);
+
+        boolean updateRoom(RoomDTO var1);
+
+        ArrayList<RoomDTO> loadAll();
+    }
+
--- src/lk/HostelManagementSystem/bo/custom/SettingBO.java
+++ src/lk/HostelManagementSystem/bo/custom/SettingBO.java
@@ -1,8 +1,9 @@
 package lk.HostelManagementSystem.bo.custom;
 
+import lk.HostelManagementSystem.bo.SuperBO;
 import lk.ijse.hostel_management_system.bo.SupperBO;
 import lk.ijse.hostel_management_system.dto.UserDTO;
 
-public interface SettingBO extends SupperBO {
+public interface SettingBO extends SuperBO {
     boolean updateUserNameAndPassword(UserDTO var1);
 }
