0537d7abdd53c7fdfca16c90cfd49c65f1f22d25
shehanKavindu <kavindushehansayakkara@gmail.com>
2023-04-22 15:24:32 +0530

    ReservationDAO

--- src/lk/HostelManagementSystem/dao/custom/ReservationDAO.java
+++ src/lk/HostelManagementSystem/dao/custom/ReservationDAO.java
@@ -1,4 +1,13 @@
 package lk.HostelManagementSystem.dao.custom;
 
-public interface ReservationDAO {
+import lk.HostelManagementSystem.entity.Reservation;
+
+import java.util.ArrayList;
+
+public interface ReservationDAO extends SQLUtil<Reservation>{
+    int getNotAvailableRoomCount(String room_type_id);
+
+    String getLastId();
+
+    ArrayList<Reservation> getAll();
 }
